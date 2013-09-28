0c7cd4516e0018738dbcdbc365df2633a4561935
swzheng <swzheng0812@gmail.com>
2013-09-28 14:34:46 -0700

    Yahoo Hack Start

---
+++ src/com/yahoo/hack/usa/main/HackMain.java
@@ -0,0 +1,14 @@
+package com.yahoo.hack.usa.main;
+
+public class HackMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yahoo Hack Start!");
+
+	}
+
+}
